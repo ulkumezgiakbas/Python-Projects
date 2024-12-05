b1d13f9aff7e66c809fc9935d7683fbaea75fef4
Ülküm Ezgi Akbaş <126782676+ulkumezgiakbas@users.noreply.github.com>
2024-12-05 14:09:51 +0300

    Add files via upload

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,16 +1,22 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
 
-#create a dictionary
-p_dictionary = {row.letter:row.code for (index,row) in data.iterrows()}
-print(p_dictionary)
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-#input and convert
-word = input("enter a word: ").upper()
-output = [p_dictionary[letter] for letter in word]
-print(output)
+def generate_phonetic():
+
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Word not in alphabet.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
 
 
 
